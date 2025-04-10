13c85a66892cb7b7d17b4f04a14e80f498afd6ab
ber1337 <ber@beaclutcher.com>
2025-04-10 18:08:48 -0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,6 @@
-// 📄 .gitignore
-node_modules/
-.env
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import pool from '../db.js';
+
+const router = express.Router();
