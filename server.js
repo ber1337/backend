4825cf91fe675a53fd0e2f81cfd59a73da15d3b1
ber1337 <ber@beaclutcher.com>
2025-04-10 18:02:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// 📁 ARQUIVOS E PASTAS
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import authRoutes from './routes/auth.js';
+import videoRoutes from './routes/videos.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/videos', videoRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor rodando na porta ${PORT}`);
+});
