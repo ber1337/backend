e1d4d16f569de717c39e3bdf8f611964c027261b
ber1337 <ber@beaclutcher.com>
2025-04-10 17:35:42 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+// raiz/
+// ├── server.js
+// ├── db.js
+// ├── package.json
+// ├── .env (⚠️ NÃO enviar para o GitHub)
+// ├── .gitignore
+// ├── routes/
+// │   ├── auth.js
+// │   └── videos.js
+// └── middleware/
+//     └── authMiddleware.js
---
+++ server.js
@@ -0,0 +1,1 @@
+// 📁 ARQUIVOS E PASTAS
